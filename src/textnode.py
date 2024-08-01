ab71a27ab4a648ce5ec0157dfd22c4f0714a5ada
mhishmeh <hishmoe@gmail.com>
2024-08-01 13:14:28 -0500

    added all files and __repr__ method

---
+++ src/textnode.py
@@ -0,0 +1,15 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text 
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, textnode2):
+        if (
+            self.text == textnode2.text and
+            self.text_type == textnode2.text_type and
+            self.url == textnode2.url
+        ): return True
+        return False
+    def __repr__(self):
+        return f"TextNode({self.text},{self.text_type},{self.url})"
