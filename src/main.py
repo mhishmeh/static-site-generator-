cdf5ddf66ea8bf65a445ccc49959d8c600bb4c7f
mhishmeh <hishmoe@gmail.com>
2024-08-01 14:22:01 -0500

    we made the final changes for the main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, text_type_italic
+
+def main():
+    node = TextNode("moe",text_type_italic,"hishmehn.com")
+    print(node)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,14 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
+
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text 
@@ -5,11 +16,11 @@ class TextNode:
         self.url = url
 
     def __eq__(self, textnode2):
-        if (
+        return (
             self.text == textnode2.text and
             self.text_type == textnode2.text_type and
             self.url == textnode2.url
-        ): return True
-        return False
+        )
     def __repr__(self):
         return f"TextNode({self.text},{self.text_type},{self.url})"
+    
